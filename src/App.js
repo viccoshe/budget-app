e5c1c4ca6e0d3554d12c274e6e91569ff0ef7537
Victoria <viccoshe@gmail.com>
2023-03-01 22:08:37 +0300

    some css are added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { createContext, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import { financeRouters } from "./routes";
+import style from "./App.module.scss"
+
+export const TContext = createContext(null);
+
 
 function App() {
+
+  const [ tstate, setTstate ] = useState([]); //Общее хранилище для всех транзакций
+  const [ count, setCount ] = useState(0);      //в виде id (или в иде генератора черещ рекрсию)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <TContext.Provider value={{
+        tstate,
+        setTstate,
+        count,
+        setCount
+      }}>
+        <div className="App">
+        <Routes>
+        {financeRouters.map(({path, Element}) =>{  //деструктуризация
+          return (<Route key={path} path={path} element={<Element/>}/>)
+        })};
+      </Routes>
     </div>
+    </TContext.Provider>
   );
 }
 
